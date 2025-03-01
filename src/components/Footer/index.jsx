cfc0882426bb5f27b991e4554db0ed1902440a93
Felizardo <felizardopaulo40@gmail.com>
2025-03-01 22:28:41 +0200

    Adiciona Contact section e o Footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,12 +1,39 @@
-import React from 'react';
+import { menu } from "../../data";
 import "./Footer.css";
+import { Link } from "react-scroll";
+import SocialHandles from "../../ui/SocialHandles";
 
 const Footer = () => {
-    return (
-        <div>
-            Footer ff
+  return (
+    <footer>
+      <div className="section__wrapper">
+        <ul className="flex__center nav">
+          {menu.map((list, index) => (
+            <Link
+              to={list.name.toLowerCase()}
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              className="nav__item"
+              key={index}
+            >
+              {" "}
+              {list.name}{" "}
+            </Link>
+          ))}
+        </ul>
+        <SocialHandles />
+        <div className="copyright">
+          <h3>Copyright &copy; Todos os direitos reservados - | 2025</h3>
+          <p className="text__muted">Construido por Felizardo Paulo Muchanga</p>
         </div>
-    )
-}
+        <div className="base__logo">
+            <h1 className="full__name shine">Felizardo Muchanga</h1>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
